3f4e2c7bc402a5ce68844f75dc0882a81a1ca465
Jay-68 <kelvinngari1995@gmail.com>
2019-11-08 23:14:39 +0300

    add location and category models for the application

--- gallery/models.py
+++ gallery/models.py
@@ -5,5 +5,28 @@ from django.db import models
 from datetime import dt
 
 # Create your models here.
-class Image(modesl.Model):
-  
+
+
+class Image(models.Model):
+    post = models.TextField()
+    image = models.ImageField(upload_to='images/')
+    pub_date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField()
+    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location)
+
+    def __str__(self):
+      return self.name
+
+
+class Category(models.Model):
+  category=models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.category
+
+class Location(models.Model):
+  location=models.CharField(max_length=30)
+
+  def __str__(self):
+    return self.location

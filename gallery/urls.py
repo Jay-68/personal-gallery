0a3eff92de23a74d51f67765c6909e0296fd70a9
Jay-68 <kelvinngari1995@gmail.com>
2019-11-09 13:34:23 +0300

    create templates for defined routes

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,6 +1,8 @@
 from django.conf.urls import url
 from . import views
 
-urlpatterns=[
-  url('^$',views)
+urlpatterns = [
+    url('^$', views.gallery_of_day, name='todaysGallery'),
+    url('^$', views.search_results, name='search_results'),
+    url(r'^location/(\d+)', views.filter_by_location, name='location')
 ]

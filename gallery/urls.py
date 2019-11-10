a673fd16663acad3a67989845cfc25ddd35cbca6
Jay-68 <kelvinngari1995@gmail.com>
2019-11-10 12:36:27 +0300

    add images and templates for home part

--- gallery/urls.py
+++ gallery/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url('^$', views.gallery_of_day, name='todaysGallery'),
-    url('^$', views.search_results, name='search_results'),
+    url('^$', views.welcome, name='welcome'),
+    url('^today/$', views.gallery_of_day, name='todaysGallery'),
+    url('^search/$', views.search_results, name='search_results'),
     url(r'^location/(\d+)', views.filter_by_location, name='location')
 ]
--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -31,7 +31,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'gallery',
+    # 'gallery',
+    'bootstrap4',
+    'gallery.apps.GalleryConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',

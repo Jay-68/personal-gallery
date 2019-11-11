99d332589145d2de7d3bae7e7c935c2d4c6f6bdb
Jay-68 <kelvinngari1995@gmail.com>
2019-11-11 14:36:29 +0300

    push to heroku

--- gallery/urls.py
+++ gallery/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns=[
     url(r'^$', views.photos, name='photos'),
     url(r'search/', views.search_results, name='search_results'),
-    url(r'location/(<location_name>\)/$', views.get_image_by_location, name='location'),
+    url(r'location/(<location_name>\)/', views.get_image_by_location, name='location'),
 ]
 
 

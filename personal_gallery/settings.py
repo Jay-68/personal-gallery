fc89f7967dae1d7d03812689ea0186891128a861
Jay-68 <kelvinngari1995@gmail.com>
2019-11-11 15:03:27 +0300

    push to heroku

--- personal_gallery/settings.py
+++ personal_gallery/settings.py
@@ -20,7 +20,6 @@ from decouple import config, Csv
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
@@ -80,7 +79,6 @@ TEMPLATES = [
 WSGI_APPLICATION = 'personal_gallery.wsgi.application'
 
 
-
 # Database
 # https://docs.djangoproject.com/en/1.11/ref/settings/#databases
 
@@ -93,8 +91,6 @@ DATABASES = {
         'PASSWORD': 'Access'
     }
 }
-
-
 
 
 # Password validation
@@ -134,7 +130,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 
-
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
@@ -148,4 +143,3 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 django_heroku.settings(locals())
-
